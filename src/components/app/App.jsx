88305e1dc1cb375cab6348a23dc748ff3316cdec
your_name <orkunkorcak@gmail.com>
2025-04-15 14:18:22 +0300

    clne last hw

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,20 @@
+import ContactForm from '../contactForm/ContactForm';
+import SearchBox from '../searchBox/SearchBox';
+import ContactList from '../contactList/ContactList';
 import css from './App.module.css'
 
 function App() {
 
   return (
     <>
+      <div className={css.container}>
+        <h1 className={css.mainTitle}>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+      </div>
     </>
-  )
+  );
 }
 
 export default App
